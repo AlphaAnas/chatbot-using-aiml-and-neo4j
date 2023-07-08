462a367d964ddbe8fc7f849c0369d496f0611b39
AlphaAnas <96944434+AlphaAnas@users.noreply.github.com>
2023-07-08 13:00:51 +0500

    Update calculator_bot.py

--- minibots/calculator_bot.py
+++ minibots/calculator_bot.py
@@ -1,7 +1,11 @@
-'code in progress' 
+'''steps :
+ code in progress right now
+'''
+
 import math
 import re
 import nltk
+import re
 def isoperand(inp):
     if inp.isdigit():
         return 1
@@ -18,6 +22,7 @@ def isoperator(inp):
         return False
 
 def infix_to_postfix(exp):
+   
     precedence={"+":1,"-":1,"*":2,"/":2,"^":3,"**":3}
     output = ''
     stack = []
@@ -49,42 +54,73 @@ def infix_to_postfix(exp):
         
 
 
-def callCalculate(name,exp):
+def callCalculate(name,exp): 
+    if 'calculate' in exp:
+        exp = exp[9:]
+    print(exp, 'previous expressino')
     mode ='radians'
     print(f'Hello {name}')
     print('Mode : radians')
-    # if any(func in exp for func in ['sin', 'cos', 'tan']):
-    #     inp = input('Press \'d\' to covert in degrees ')
-    #     print('------- Press any key to continue --------')
-    #     if inp =='d':
-    #         mode = 'degree'
- 
     parts = re.split(r'([+\-*^/])',exp)
     parts = [part.strip().lower() for part in parts if part.strip()] 
     #remove one sided brackets:
     for i, ele in enumerate(parts):
-        if ele[-1]==')':
-            bracket = ele[-1]
-            no =ele[:-1]
-            parts.insert(i,no)
-            parts.insert(i+1,bracket) 
-        elif ele[0]=='(':
-            bracket =ele[0]
-            no =ele[1:]
-            parts.insert(i, bracket)
-            parts.insert(i+1,no)
+        if len(ele) >1:
+            if ele[-1]==')' and not(ele[0] == '('):
+                bracket = ele[-1]
+                no =ele[:-1]
+                parts[i]= no
+                parts.insert(i+1,bracket) 
+            elif ele[0]=='(' and not(ele[-1]==')'):
+                bracket =ele[0]
+                no =ele[1:]
+                parts[i] = bracket
+                parts.insert(i+1,no)
+ 
   
            
-    # exp = infix_to_postfix(parts)
+    postfix = infix_to_postfix(parts)
 
-    print(parts,'the expression ')
-#     if "calculate" in exp:
-#         exp = exp[9:] #after calculate_
-#         parts = re.split(r'([+\-*/])',exp) #splits the expression
-#         print(parts,'fjfj')
+    print(postfix,'the expression ')
+
+    ans = evaluatePostfix(postfix)
+    print(ans, 'final')
+
+def evaluatePostfix(postfix):
+    ans = []
+    operators = ['+', '-', '*' ,'/', '^']
+    Spostfix = split(postfix)
+
+   
+    for ele in Spostfix:
+
+        # If the scanned character is an operand
+        # (number here) push it to the stack
+        if ele.isdigit():
+            ans.push(ele)
+        elif any(func in ele for func in ['sin', 'tan', 'cos']):
+            ans.push(ele)
+
+        elif ele in operators:
+            val1 = ans.pop()
+            val2 = ans.pop()
+            if any(func in val1 for func in ['sin', 'tan', 'cos']):
+                val1 = calc_trig(ele)
+            if any(func in val2 for func in ['sin', 'tan', 'cos']):
+                val2 = calc_trig(ele)
+        
+            ans.append(str(eval(val2 + ele + val1)))
+
+    return ans
+
 
        
-     
+def split(postfix):
+    out =[]
+    for i,ele in enumerate(postfix):
+        if ele.isalpha():
+            out.append(postfix[i:i+7]) # extract the "sin(angle)"
+        e       
         
 #         print(parts, 'bd wala')
 
@@ -132,13 +168,13 @@ def callCalculate(name,exp):
 #     #     exp = exp[5:] # after solve_
 #     #     return eval(exp)
     
-# def calc_trig(theta,opr):
-#     if opr.lower() =='sin':
-#         return math.sin(theta) 
-#     elif opr.lower() =='cos':
-#         return math.cos(theta) 
-#     elif opr.lower() =='tan':
-#         return math.tan(theta)# arc tan etc 'll be added
+def calc_trig(theta,opr):
+    if opr.lower() =='sin':
+        return math.sin(theta) 
+    elif opr.lower() =='cos':
+        return math.cos(theta) 
+    elif opr.lower() =='tan':
+        return math.tan(theta)# arc tan etc 'll be added
     
 
 
